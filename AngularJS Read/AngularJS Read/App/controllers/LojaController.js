0e730e17860146922f872139923fe64518d5f51c
Reds <Reds@REDS-PC>
2016-06-13 20:45:50 -0300

    iniciando API REST

---
+++ AngularJS Read/AngularJS Read/App/controllers/LojaController.js
@@ -0,0 +1,21 @@
+﻿angular.module("aplicacao").controller("Loja", function ($scope)
+{
+            $scope.loja = {
+                Id: 1,
+                Nome: "Loja do zé",
+                Categoria: "bar",
+                Numero: "532",
+                Rua: "Barmatelo Luiz",
+                Url: "https://media-cdn.tripadvisor.com/media/photo-s/06/26/f7/d8/bar-do-ze.jpg"
+            }
+            $scope.loja2 = {
+                Id: 1,
+                Nome: "loja do jão",
+                Categoria: "bar",
+                Numero: "287",
+                Rua: "Maria das rosas",
+                Url: ""
+            }
+            
+        }
+    );
